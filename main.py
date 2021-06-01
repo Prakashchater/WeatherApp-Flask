f0682d16c024c4bc9b33e4b6ef5e73048575b532
Prakash <prakashchater@gmail.com>
2021-06-01 21:24:05 +0530

    commit changes

--- main.py
+++ main.py
@@ -8,39 +8,59 @@ weather_api_key = "592dd33a1092d9eca5c1b173c4d88ee9"
 
 app = Flask(__name__)
 
-# app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///weather.db'
-# app.config['SECRET_KEY'] = "qwertyuioplkjhgfdsa"
-# app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+def get_weather_data(city):
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=592dd33a1092d9eca5c1b173c4d88ee9"
+    response = requests.get(url=weather_url)
+    return response
 
-# db = SQLAlchemy(app)
+# @app.route('/', methods=["GET","POST"])
+# def home():
+#     if request.method == "POST":
+#         new_city_data = get_weather_data(city)
+#         city = request.form.get('city')
+#     else:
+#         city = 'jamshedpur'
 
+#     weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={city.title()}&units=metric&appid=592dd33a1092d9eca5c1b173c4d88ee9"
+#     response = requests.get(weather_url).json()
+#     weather = {
+#     'city': response['name'],
+#     'temperature': response['main']['temp'],
+#     'weather': response['weather'][0]['description'],
+#     'icon': response['weather'][0]['icon'],
+#     'min_temp': response['main']['temp_min'],
+#     'max_temp': response['main']['temp_max'],
+#     'humidity': response['main']['humidity'],
+#     'time': datetime.now().strftime('%H:%M')
+#         }
+#     return render_template('index.html', weather=weather)
 
-# class City(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=False)
-
-# db.create_all()
-
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=["GET","POST"])
 def home():
-    if request.method == "POST":
-        city = request.form['city']
-    else:
-        correct = False
-        
-    weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={city.title()}&units=metric&appid=592dd33a1092d9eca5c1b173c4d88ee9"
-    response = requests.get(url=weather_url).json()
-    weather_data = {
-        'city': response['name'],
-        'temperature': response['main']['temp'],
-        'weather': response['weather'][0]['description'],
-        'icon': response['weather'][0]['icon'],
-        'min_temp': response['main']['temp_min'],
-        'max_temp': response['main']['temp_max'],
-        'humidity': response['main']['humidity'],
-        'time': datetime.now().strftime('%H:%M')
+    err_msg = ' '
+    city = request.form.get('city')
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=592dd33a1092d9eca5c1b173c4d88ee9"
+    response = requests.get(weather_url).json()
+    weather = {
+    'city': response['name'],
+    'temperature': response['main']['temp'],
+    'weather': response['weather'][0]['description'],
+    'icon': response['weather'][0]['icon'],
+    'min_temp': response['main']['temp_min'],
+    'max_temp': response['main']['temp_max'],
+    'humidity': response['main']['humidity'],
+    'time': datetime.now().strftime('%H:%M')
         }
-    return render_template('index.html', weather = weather_data, correct=True)
+    if request.method == "POST":
+        if response['cod'] == 200:
+            city = request.form['city']
+            # return render_template('index.html', weather=weather)
+        else:
+            err_msg = "Not found"
+            return redirect(url_for('home'))
+    return render_template('index.html', weather=weather)
+
+    
 
 
 if __name__ == "__main__":
