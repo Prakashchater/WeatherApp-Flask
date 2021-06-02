0483e0a75bc56c4f563e8c212afe14a5135cebbb
Prakash <prakashchater@gmail.com>
2021-06-02 19:25:30 +0530

    commit changes

--- main.py
+++ main.py
@@ -13,55 +13,35 @@ def get_weather_data(city):
     response = requests.get(url=weather_url)
     return response
 
-# @app.route('/', methods=["GET","POST"])
-# def home():
-#     if request.method == "POST":
-#         new_city_data = get_weather_data(city)
-#         city = request.form.get('city')
-#     else:
-#         city = 'jamshedpur'
-
-#     weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={city.title()}&units=metric&appid=592dd33a1092d9eca5c1b173c4d88ee9"
-#     response = requests.get(weather_url).json()
-#     weather = {
-#     'city': response['name'],
-#     'temperature': response['main']['temp'],
-#     'weather': response['weather'][0]['description'],
-#     'icon': response['weather'][0]['icon'],
-#     'min_temp': response['main']['temp_min'],
-#     'max_temp': response['main']['temp_max'],
-#     'humidity': response['main']['humidity'],
-#     'time': datetime.now().strftime('%H:%M')
-#         }
-#     return render_template('index.html', weather=weather)
-
 @app.route('/', methods=["GET","POST"])
 def home():
     err_msg = ' '
     city = request.form.get('city')
-    weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=592dd33a1092d9eca5c1b173c4d88ee9"
-    response = requests.get(weather_url).json()
-    weather = {
-    'city': response['name'],
-    'temperature': response['main']['temp'],
-    'weather': response['weather'][0]['description'],
-    'icon': response['weather'][0]['icon'],
-    'min_temp': response['main']['temp_min'],
-    'max_temp': response['main']['temp_max'],
-    'humidity': response['main']['humidity'],
-    'time': datetime.now().strftime('%H:%M')
+    try: 
+        weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=592dd33a1092d9eca5c1b173c4d88ee9"
+        response = requests.get(url=weather_url).json()
+        weather = {
+        'city': response['name'],
+        'temperature': response['main']['temp'],
+        'weather': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon'],
+        'min_temp': response['main']['temp_min'],
+        'max_temp': response['main']['temp_max'],
+        'humidity': response['main']['humidity'],
+        'time': datetime.now().strftime('%H:%M'),
         }
-    if request.method == "POST":
-        if response['cod'] == 200:
-            city = request.form['city']
-            # return render_template('index.html', weather=weather)
-        else:
-            err_msg = "Not found"
-            return redirect(url_for('home'))
-    return render_template('index.html', weather=weather)
-
-    
-
+        return render_template('index.html', weather=weather)
+    except KeyError:
+        if request.method == "POST":
+            city = request.form.get('city')
+            if response['cod'] == 200:
+                return render_template('index.html', weather=weather)
+            else:
+                err_msg = "Not found"
+    except UnboundLocalError:
+        return render_template('index.html', weather=weather)
+    finally:
+        return render_template('index.html', weather=weather)
 
 if __name__ == "__main__":
     app.run(debug=True)
